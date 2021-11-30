ebf765411630e039c4adda1786d169fb2a38401c
quentin-blechet <quentin.blechet@gmail.com>
2021-11-30 10:08:12 +0100

    fix

--- load.js
+++ load.js
@@ -2,8 +2,10 @@ const q= document.getElementById('query');
 const q2= document.getElementById('query2');
 const viewer = $3Dmol.createViewer( element, config );
 const viewer2 = $3Dmol.createViewer( element2, config );
-const seq1 = document.getElementById('seq1');
-const seq2 = document.getElementById('seq2');
+const idseq1 = document.getElementById('seq1');
+const idseq2 = document.getElementById('seq2');
+let seq1;
+let seq2;
 
 function submitted() {
     viewer.clear();
@@ -24,6 +26,8 @@ function submit_seq(){
     let ind1 = histo_id.indexOf(q.value.toUpperCase());
     let ind2 = histo_id.indexOf(q2.value.toUpperCase());
     console.info(ind1,ind2);
-    seq1.innerHTML = histo_prot[ind1]["pdb"]["sequence"];
-    seq2.innerHTML = histo_prot[ind2]["pdb"]["sequence"];
+    seq1 = histo_prot[ind1]["pdb"]["sequence"];
+    seq2 = histo_prot[ind2]["pdb"]["sequence"];
+    idseq1.innerHTML = seq1;
+    idseq2.innerHTML = seq2;
 }
